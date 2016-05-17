c94b8d8f7a259decfd081900859947157a552a14
Justin Hammond <justin.hammond@rackspace.com>
2016-05-17 13:48:17 -0500

    Bumping version to 0.24

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name='os_networksv2_python_novaclient_ext',
-    version='0.23',
+    version='0.24',
     description='Adds rackspace networks support to python-novaclient',
     long_description=open('README.rst').read(),
     author='Rackspace',
